14d80b291c5ed21eb871d234d608c3469bfca78f
Erica Roachford-Silverman <naynay288@gmail.com>
2023-10-31 15:53:05 -0400

    Assignment 3 Push

---
+++ assignments/HAUGH/3/cookie.cmd
@@ -0,0 +1,100 @@
+curl -ILsk alexa.com > alexa.com.txt						
+curl -ILsk alibaba.com > alibaba.com.txt									
+curl -ILsk aljazeera.com > aljazeera.com.txt									
+curl -ILsk aol.com > aol.com.txt									
+curl -ILsk ap.org > ap.org.txt									
+curl -ILsk bp2.blogger.com > bp2.blogger.com.txt									
+curl -ILsk britannica.com > britannica.com.txt									
+curl -ILsk buzzfeed.com > buzzfeed.com.txt									
+curl -ILsk canada.ca  > canada.ca.txt									
+curl -ILsk cbc.ca > cbc.ca.txt									
+curl -ILsk cnbc.com > cnbc.com.txt									
+curl -ILsk cnil.fr > cnil.fr.txt								
+curl -ILsk code.google.com > code.google.com.txt								
+curl -ILsk cointernet.com.co > cointernet.com.co.txt									
+curl -ILsk cornell.edu > cornell.edu.txt						
+curl -ILsk dailymotion.com> dailymotion.com.txt				
+curl -ILsk dailystar.co.uk > dailystar.co.uk.txt		
+curl -ILsk disney.com > disney.com.txt									
+curl -ILsk doi.org > doi.org.txt								
+curl -ILsk draft.blogger.com > draft.blogger.com.txt								
+curl -ILsk dreamstime.com > dreamstime.com.txt					
+curl -ILsk drive.google.com > drive.google.com.txt				
+curl -ILsk dropbox.com > dropbox.com.txt	
+curl -ILsk dropcatch.com > dropcatch.com.txt
+curl -ILsk ebay.com > ebay.com.txt
+curl -ILsk es.wikipedia.org > es.wikipedia.org.txt
+curl -ILsk fastcompany.com> fastcompany.com.txt
+curl -ILsk fifa.com> fifa.com.txt
+curl -ILsk freepik.com > freepik.com.txt
+curl -ILsk ft.com> ft.com.txt
+curl -ILsk g.co> g.co.txt
+curl -ILsk gettyimages.com> gettyimages.com.txt
+curl -ILsk godaddy.com > godaddy.com.txt
+curl -ILsk goodreads.com >goodreads.com.txt
+curl -ILsk google.co.jp> google.co.jp.txt
+curl -ILsk google.co.th> google.co.th.txt
+curl -ILsk google.it > google.it.txt
+curl -ILsk gooyaabitemplates.com > gooyaabitemplates.com.txt
+curl -ILsk guardian.co.uk > guardian.co.uk.txt
+curl -ILsk icann.org > icann.org.txt
+curl -ILsk ign.com > ign.com.txt								
+curl -ILsk imageshack.us > imageshack.us.txt									
+curl -ILsk iso.org> iso.org.txt								
+curl -ILsk it.wikipedia.org > it.wikipedia.org.txt									
+curl -ILsk ja.wikipedia.org > ja.wikipedia.org.txt									
+curl -ILsk kickstarter.com > kickstarter.com.txt								
+curl -ILsk last.fm > last.fm.txt								
+curl -ILsk linkedin.com > linkedin.com.txt									
+curl -ILsk m.wikipedia.org > m.wikipedia.org.txt								
+curl -ILsk marketingplatform.google.... > marketingplatform.google.....txt									
+curl -ILsk medium.com > medium.com.txt									
+curl -ILsk metro.co.uk > metro.co.uk.txt									
+curl -ILsk microsoft.com > microsoft.com.txt									
+curl -ILsk mozilla.com > mozilla.com.txt 									
+curl -ILsk msn.com > msn.com.txt								
+curl -ILsk nationalgeographic.com > nationalgeographic.com.txt									
+curl -ILsk networkadvertising.org > networkadvertising.org.txt									
+curl -ILsk news.com.au > news.com.au.txt									
+curl -ILsk newyorker.com > newyorker.com.txt									
+curl -ILsk nl.wikipedia.org > nl.wikipedia.org.txt									
+curl -ILsk offset.com > offset.com.txt									
+curl -ILsk ok.ru > ok.ru.txt									
+curl -ILsk pbs.org > pbs.org.txt								
+curl -ILsk picasa.google.com > picasa.google.com.txt									
+curl -ILsk picasaweb.google.com > picasaweb.google.com.txt									
+curl -ILsk pixabay.com > pixabay.com.txt									
+curl -ILsk plus.google.com > plus.google.com.txt								
+curl -ILsk prestashop.com > prestashop.com.txt									
+curl -ILsk redbull.com > redbull.com.txt									
+curl -ILsk reddit.com > reddit.com.txt
+curl -ILsk repubblica.it > repubblica.it.txt									
+curl -ILsk reuters.com > reuters.com.txt									
+curl -ILsk sendspace.com > sendspace.com.txt									
+curl -ILsk shopify.com > shopify.com.txt									
+curl -ILsk soratemplates.com > soratemplates.com.txt									
+curl -ILsk spotify.com > spotify.com.txt									
+curl -ILsk surveymonkey.com > surveymonkey.com.txt 									
+curl -ILsk t.me> t.me.txt									
+curl -ILsk target.com > target.com.txt									
+curl -ILsk theguardian.com > theguardian.com.txt								
+curl -ILsk tinyurl.com  > tinyurl.com.txt									
+curl -ILsk trustpilot.com > trustpilot.com.txt									
+curl -ILsk usda.gov > usda.gov.txt									
+curl -ILsk usgs.gov > usgs.gov.txt									
+curl -ILsk vk.com > vk.com.txt									
+curl -ILsk walmart.com > walmart.com.txt									
+curl -ILsk washingtonpost.com > washingtonpost.com.txt									
+curl -ILsk webmd.com > webmd.com.txt									
+curl -ILsk webnode.page > webnode.page.txt									
+curl -ILsk whitehouse.gov > whitehouse.gov.txt									
+curl -ILsk wiley.com > wiley.com.txt									
+curl -ILsk wordreference.com > wordreference.com.txt									
+curl -ILsk www.blogger.com > www.blogger.com.txt									
+curl -ILsk www.gov.br > www.gov.br.txt									
+curl -ILsk www.over-blog.com > www.over-blog.com.txt									
+curl -ILsk www.wikipedia.org > www.wikipedia.org.txt									
+curl -ILsk www.yahoo.com > www.yahoo.com.txt									
+curl -ILsk yahoo.co.jp > yahoo.co.jp.txt									
+curl -ILsk yale.edu > yale.edu.txt									
+curl -ILsk zoom.us > zoom.us.txt									
